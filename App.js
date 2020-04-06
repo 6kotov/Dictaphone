b823ac367a2d31ea632f797d903860d16a9d4d78
6kotov <66kotov@gmail.com>
2020-04-06 19:25:44 +0200

    Simple recording & playing functions implemented.

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  StyleSheet,
+  Text,
+  Slider,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 import { Audio } from "expo-av";
 import { Asset } from "expo-asset";
+// import Slider from "@react-native-community/slider";
 import * as Premissions from "expo-permissions";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 class Icon {
   constructor(module, width, height) {
@@ -13,17 +22,26 @@ class Icon {
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get("window");
 const BACKGROUND_COLOR = "#FFF8ED";
 const LIVE_COLOR = "#FF0000";
-const DISABLED_OPACITY = 0.5;
+const DISABLED_OPACITY = 0.7;
 
 export default function App() {
   const [recordPremission, setRecordPremission] = useState(false);
   const [loading, setLoading] = useState(false);
   const [volume, setVolume] = useState(1.0);
-  const [recordDuration, setRecordDuration] = useState(null);
-  const [recordStatus, setRecordStatus] = useState(false);
-  const recordButton = recordStatus ? "Stop recording." : "Start recording.";
-  const [recording, setRecording] = useState(new Audio.Recording());
-  const recordSwich = recordStatus ? stopRecord : startRecord;
+  const [isMuted, setIsMuted] = useState(false);
+  const [soundDuration, setSoundDuration] = useState(null);
+  const [soundPosition, setSoundPosition] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [seekPosition, setSeekPosition] = useState(null);
+  const [isSeeking, setIsSeeking] = useState(0);
+  const [recorder, setRecorder] = useState(new Audio.Recording());
+  const [soundPlayer, setSoundPlayer] = useState(null);
+  const [recColor, setRecColor] = useState("black");
+  const [isPlaybackAllowed, setIsPlaybackAllowed] = useState(false);
+  const recordButton = isRecording ? "Stop recording." : "Start recording.";
+  const recordSwich = isRecording ? stopRecord : startRecord;
+  const playSwich = isPlaying ? pausePlay : startPlay;
 
   useEffect(() => {
     askForPremissions();
@@ -35,22 +53,18 @@ export default function App() {
   }
 
   async function startRecord() {
+    setRecColor("red");
     try {
       if (!recordPremission) {
         askForPremissions();
       }
-      if (recording === null) {
-        setRecording(new Audio.Recording());
-      }
 
-      setRecordStatus(true);
-      await recording.prepareToRecordAsync(
-        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      setIsRecording(true);
+      await recorder.prepareToRecordAsync(
+        Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY
       );
       console.log("Recording Prepared");
-      console.log(recording.getURI());
-      recording.getURI();
-      await recording.startAsync();
+      await recorder.startAsync();
       console.log("Record Stared");
       // You are now recording!
     } catch (error) {
@@ -59,22 +73,93 @@ export default function App() {
   }
 
   async function stopRecord() {
+    setRecColor("black");
     try {
-      recording.stopAndUnloadAsync();
-      setRecordStatus(false);
-      setRecording(null);
-      setRecording(new Audio.Recording());
+      await recorder.stopAndUnloadAsync();
+      const { sound, status } = await recorder.createNewLoadedSoundAsync(
+        {},
+        null
+      );
+
+      setIsRecording(false);
+      setSoundPlayer(sound);
+      setRecorder(null);
+      setRecorder(new Audio.Recording());
       console.log("Recording stopped");
     } catch (error) {
       console.log("Error? cant stop recording", error);
     }
   }
 
+  async function startPlay() {
+    setIsPlaying(true);
+    await soundPlayer.playAsync();
+  }
+
+  async function pausePlay() {
+    setIsPlaying(false);
+    await soundPlayer.pauseAsync();
+  }
+
+  async function stopPlay() {
+    setIsPlaying(false);
+    await soundPlayer.stopAsync();
+  }
+
   return (
     <View style={styles.container}>
       <Text>Recorder</Text>
-      <View>
-        <Button onPress={() => recordSwich()} title={recordButton} />
+
+      <TouchableOpacity
+        onPress={() => recordSwich()}
+        activeOpacity={DISABLED_OPACITY}
+      >
+        <MaterialCommunityIcons
+          name="record-rec"
+          size={60}
+          style={styles.recordButton}
+          color={recColor}
+        />
+      </TouchableOpacity>
+      <View style={styles.playBackContainer}>
+        <Slider
+          style={styles.playbackSlider}
+          // trackImage={ICON_TRACK_1.module}
+          // thumbImage={ICON_THUMB_1.module}
+          // value={this._getSeekSliderPosition()}
+          // onValueChange={this._onSeekSliderValueChange}
+          // onSlidingComplete={this._onSeekSliderSlidingComplete}
+          disabled={!isPlaybackAllowed || loading}
+        />
+        <TouchableOpacity
+          activeOpacity={DISABLED_OPACITY}
+          style={styles.player}
+        >
+          {!isPlaying ? (
+            <MaterialCommunityIcons
+              name="play"
+              onPress={() => playSwich()}
+              size={60}
+              color="black"
+              disabled={!isPlaybackAllowed || loading}
+            />
+          ) : (
+            <MaterialCommunityIcons
+              name="pause"
+              onPress={() => playSwich()}
+              size={60}
+              color="black"
+              disabled={!isPlaybackAllowed || loading}
+            />
+          )}
+          <MaterialCommunityIcons
+            name="stop"
+            onPress={() => stopPlay()}
+            size={60}
+            color="black"
+            disabled={!isPlaybackAllowed || loading}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -85,6 +170,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
+  playbackSlider: {
+    width: DEVICE_WIDTH / 2.0,
+  },
+  recordButton: {},
+  player: {
+    flexDirection: "row",
+  },
+  playBackContainer: {},
 });
